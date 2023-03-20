7d2abeaeb5f13796f230e9a06c8ee3446c403bbf
Artem Ulianytskyi <darthdeir@gmail.com>
2023-03-20 17:44:02 +0200

    Commit Vas05

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
-<<<<<<< HEAD
         System.out.print("Введи будь-який текст: ");
         String text = scanner.nextLine();
 
-
         System.out.println("Ти надрукував: " + text);
-    }
-
-
-=======
-
-        String text = scanner.nextLine();
+        text = scanner.nextLine();
 
 
         System.out.println(text);
     }
->>>>>>> f50f39b (Initial commit)
 }
